a285efd7f8dad8fc4868505bfc4aca550035e7d9
Messsaoud Rostane <messaoud.rostane6@gmail.com>
2024-07-23 11:07:59 +0200

    Mise en page : création de la maquette header/main/aside/footer avec les composant angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    AsideComponent,
+    FooterComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule
